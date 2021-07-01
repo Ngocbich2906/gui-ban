c48eccbb5269b940dd6fff57b8be51eb423e2287
Ngocbich2906 <86782306+Ngocbich2906@users.noreply.github.com>
2021-07-01 20:04:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bạn à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà mình chưa hiểu sao mình cute thế :))`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Bạn buộc phải công nhận Bích rất cute hihi 😙',
+    desc: 'Bạn thấy Ngọc Bích cute khummm? ',
+    btnYes: 'Of course Bích siêu đáng yêu <33',
+    btnNo: 'Không, Bạn à à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Bích cute thế <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Công nhận Bích cute thiệt <33333333',
+    mess: 'Mình biết mà 🥰. Cảm ơn bạn nhiều nhiều 😘😘',
+    messDesc: 'Bạn vô duyên thế.',
+    btnAccept: 'Xin lỗi :))',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
